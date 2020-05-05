f104bfe21f3c0432e53ef977facec86a300ac807
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-05 21:15:03 +0300

    SearchBarin TextInput luotu.

--- src/components/SeachBar.js
+++ src/components/SeachBar.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import { Feather} from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const SearchBar = () =>{
     return(
-        <View style={styles.background}>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name= 'search' size={30}></Feather>
+            <TextInput style= {styles.inputStyle}placeholder= "Search"></TextInput>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: 'rgb(220,220,220)',
         height: 50,
         borderRadius: 10,
-        marginHorizontal: 15
-        
-        
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        //borderColor: 'black',
+        //borderWidth: 2,
+        flex: 1
     }
+
 
 });
 
