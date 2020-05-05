67bf95c32e75b5d62404614813c05efdf8f377dd
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-05 21:28:15 +0300

    Style fikailua. Iconiin oma tyyli.

--- src/components/SeachBar.js
+++ src/components/SeachBar.js
@@ -7,7 +7,9 @@ import { TextInput } from 'react-native-gesture-handler';
 const SearchBar = () =>{
     return(
         <View style={styles.backgroundStyle}>
-            <Feather name= 'search' size={30}></Feather>
+            <Feather name= 'search'
+                    style= {styles.iconStyle}
+            />
             <TextInput style= {styles.inputStyle}placeholder= "Search"></TextInput>
         </View>
     );
@@ -18,13 +20,24 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgb(220,220,220)',
         height: 50,
         borderRadius: 10,
+        marginTop: 10,
         marginHorizontal: 15,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        
     },
     inputStyle: {
         //borderColor: 'black',
         //borderWidth: 2,
+        fontSize: 18,
+        left: 0,
         flex: 1
+    },
+
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: "center",
+        marginHorizontal: 10
+
     }
 
 
