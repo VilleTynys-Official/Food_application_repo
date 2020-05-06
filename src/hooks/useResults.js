ae4f3fb0d1067d72e55b06e184406af076d0ec4e
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-06 14:35:20 +0300

    apin "business logiikka" siirretty onnistuneesti useResults.js hookkiiin.

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -8,9 +8,43 @@ import yelp from '../api/yelp';
 
 
 export default () =>{
+        const [results, setResults] =useState([]);
+        const [errorMessage, setErrorMessage] = useState('');
+        
+        //async funktio joka hakee yelpistä tulokset arrayna.
+        //params:    asettaa lisäsanoja kyselyyn (käyttämällä ?merkkiä..)
+        const searchApi = async(searchTerm) =>{
 
 
-    
+        try{
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 10,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);     // array asetetaan vastaus muuttujaan.
+            
+        } catch(err){
+            setErrorMessage('Something went wrong');
+        }
+    };
 
 
-}
+    //BAAAD CODE:
+    //Call searchApi when component is first rendered.
+    //searchApi('pasta');       >>>parent renders multiple times so this would also..
+
+    //GOOD CODE:
+    //käytä useEffect() joka renderöityy vain kerran.
+    useEffect(()=>{
+        searchApi('pasta');
+    }, []);
+
+    return [searchApi, results, errorMessage];
+
+
+
+
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SeachBar'
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 /*
 DAY 4
@@ -14,8 +15,16 @@ DAY 4
         callback to manage states
 
     FLOW OF THE SEARCH SCREEN
-        LIFECYCLE FUNCTIONS
+        RENDERING FUNCTIONS
             useEffect (update e.g. when first rendered or first rendered and when value inside the array changes..)
+        EXTRACT HOOKS
+            "get the business logic out of the SearchScreen"
+            "remove everything api related to a hook that can be added where ever the app needs api results."
+
+            1. move the api related code to hook file
+            2. update the connection of the rendered view to the hook.
+                return the objects you need from the hook..
+                connect the hook to the files that consume it   
 
 */ 
 
@@ -26,46 +35,7 @@ DAY 4
 //SearchBar uses the onTermChange and changes the term here.
 const SearchScreen = () =>{
     const [term, setTerm] = useState('');
-    const [results, setResults] =useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    //async funktio joka hakee yelpistä tulokset arrayna.
-    //params:    asettaa lisäsanoja kyselyyn (käyttämällä ?merkkiä..)
-    const searchApi = async(searchTerm) =>{
-
-
-    try{
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 10,
-                term: searchTerm,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses);     // array asetetaan vastaus muuttujaan.
-        
-    } catch(err){
-        setErrorMessage('Something went wrong');
-    }
-
-
-    };
-
-
-
-
-    
-//BAAAD CODE:
-//Call searchApi when component is first rendered.
-//searchApi('pasta');       >>>parent renders multiple times so this would also..
-
-//GOOD CODE:
-//käytä useEffect() joka renderöityy vain kerran.
-useEffect(()=>{
-    //console.log('useEfffect eka kerta')
-    searchApi('pasta');
-}, []);
-
+    const [searchApi, results, errorMessage] = useResults(); //tuodaan propsit hookista
 
     return(
         <View>
@@ -79,7 +49,7 @@ useEffect(()=>{
             <Text>we have found {results.length} results</Text>
         </View>
     )
-}
+};
 
 const styles = StyleSheet.create({});
 
