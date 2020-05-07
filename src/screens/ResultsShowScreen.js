570257f16d926cb8eb50d522b1baa55eb7a5e08c
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-07 14:24:56 +0300

    Lisätty business logiikka: haetaan id:llä apista tiedot, jotka päivittyvät resulttiin. Tämän jälkeen resultista tulostetaan valitun id:n nimi.

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { useState } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { useState, useEffect } from 'react';
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
+import yelp from '../api/yelp';
 
 
-const ResultsShowScreen = ({navigation}) =>{
-    const id= navigation.getParam('id');
+
+const ResultsShowScreen = ({navigation}) => {
+    const [result, setResult] = useState(null); //for objects the default value is 'null'
+    const id = navigation.getParam('id');
+
     console.log(id)
 
+    //hakee datan ja asettaa sen result muuttujaan.
+    const getResult = async (id)=>{
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+        console.log(response.data)
+    };
+
+    //määrää et getResult suoritetaan vain kerran.
+    useEffect(() =>{getResult(id);}, []);
+
+    if (!result){           //ei näytetä mitään jos result on null.
+        return null;
+    }
 
     return(
    
         <View>
-            <Text>results show screen</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo)=> photo}
+                renderItem={( {item} )=>{
+                    return (
+                        <Image
+                            style= {styles.Image}
+                            source= {{ uri: item}}></Image>
+                    )
+
+
+                }}
+            ></FlatList>
         </View>
             
 
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height:200,
+        width: 300
+        
+    }
+});
 
 export default ResultsShowScreen;
